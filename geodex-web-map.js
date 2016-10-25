d0914c5be7db5e288a406a71d12ee3ed49265ada
Ben Schroeder <btpschroeder@gmail.com>
2016-10-25 13:46:35 -0500

    Search option for within/intersect

--- geodex-web-map.js
+++ geodex-web-map.js
@@ -1,8 +1,3 @@
-// set up query url (map service)
-var geodexQuery = L.esri.query({
-    url: 'http://webgis.uwm.edu/arcgisuwm/rest/services/AGSL/GeodexPolandOnly/MapServer/0'
-});
-
 // when document is ready...
 $(document).ready(function(){
     makeTheMap(); //... render the map
@@ -17,39 +12,48 @@ $(document).ready(function(){
         
         // initialize the map
         var theMap = L.map('map') // draw the map in the element with an id of "map"
-            .setView([52.230618, 19.364278], // by default, center to Kutno, Poland...
+            .setView([43.038902, -87.906474], // by default, center to Milwaukee...
             6); // ...with a zoom level of 6
             
         // disable "search current extent" button by default
         $('#search-current-extent').prop('disabled', 'disabled');
             
         // this variable holds the minimum zoom level needed to activate "search current extent" button
-        var minExtentZoom = 10;
+        var minExtentZoom = 9;
             
-        // add basemap: currently OpenStreetMap.Mapnik
-        var basemapLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        // add individual basemap layers
+        var osmBasemap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             minZoom: 4,
             maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> | Powered by <a href="http://esri.com/">Esri</a>'}); 
-        basemapLayer.addTo(theMap);
-        
-        /*
-        // add geodex features to the map via map service on uwm's arcgis server
-        var geodexFeatures = L.esri.featureLayer({
-            url: 'http://webgis.uwm.edu/arcgisuwm/rest/services/AGSL/GeodexPolandOnly/MapServer/0'
+            attribution: 'Basemap &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        });            
+        var esriWorldImageryBasemap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+            attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
         });
-        geodexFeatures.addTo(theMap);
-        
-        // bind popups to geodex features, displayed upon clicking feature layer
-        geodexFeatures.bindPopup(function(data) {
-            return L.Util.template(
-                // this is the HTML rendered when somebody clicks on a feature
-                // put fields from the dataset between curly braces {}
-                '<strong>{LOCATION}</strong><br /><em>{SERIES_TIT}</em><br />Date: {DATE}',
-                data.feature.properties
-            );
+        var esriWorldTopoMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+            attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
         });
-        */
+        var esriWorldStreetMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+            attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+        });
+            
+        // add the default basemap to the map
+        esriWorldTopoMap.addTo(theMap);
+        
+        // add a layer group containing all the basemap layers; then add the layer group to the map with controllers
+        var basemaps = {
+            "Esri World Topographic Map": esriWorldTopoMap,
+            "Esri World Imagery": esriWorldImageryBasemap,
+            "Esri World Street Map": esriWorldStreetMap,
+            "OSM Mapnik": osmBasemap
+        }
+        var basemapsControl = L.control.layers(basemaps).addTo(theMap);
+        
+        // is there currently a feature outline on the map?
+        var outlineOnMap = false;
+        
+        // a group to hold the feature outlines
+        var temporaryLayerGroup;
         
         // add esri geocoder to map
         var geocoderControl = L.esri.Geocoding.geosearch();
@@ -79,13 +83,22 @@ $(document).ready(function(){
             var ne = currentMapBounds._northEast; // assign the northeast boundary to a variable
             var queryBounds = L.latLngBounds(sw, ne); // combine the boundaries into a leaflet latlong object
             
-            geodexQuery.intersects(queryBounds); // set up the query
-            // note that 'intersects' means features need only be partially included in the map extent to show up in the search results
-            // other query methods are possible: https://esri.github.io/esri-leaflet/api-reference/tasks/query.html
+            var geodexSearchQuery = L.esri.query({
+                url: 'http://webgis.uwm.edu/arcgisuwm/rest/services/AGSL/GeodexWebMapService/MapServer/0'
+            });
             
-            geodexQuery.run(function(error, featureCollection, response){
+            if ($('#search-intersect').prop("checked")) {
+                console.log("intersects search");
+                geodexSearchQuery.intersects(queryBounds);
+                // note that 'intersects' means features need only be partially included in the map extent to show up in the search results
+            } else if ($('#search-within').prop("checked")){
+                console.log("within search");
+                geodexSearchQuery.within(queryBounds);
+                // whereas this returns only features ENTIRELY WITHIN the current extent
+            }
+            
+            geodexSearchQuery.run(function(error, featureCollection, response){
                 // pass all of the query results to the displaySearchResults function
-                console.log(featureCollection.features);
                 displaySearchResults(featureCollection.features);
             });
         });
@@ -95,9 +108,11 @@ $(document).ready(function(){
         /////////////////////////////////////////////////
 
         function displaySearchResults(s) { // in this function, the s parameter refers to search results passed in
+        
+            var temporaryLayer;
 
             var resultsHtml = // all of the output html will be stored in this variable
-                '<p><button type="button" class="btn btn-default" id="clear-results">Clear search results</button>'
+                '<p><button type="button" class="btn btn-default" id="clear-results">Clear search results</button>' +
                 '<p>' +
                 'Found <strong>' + s.length + '</strong> matching results.' +
                 '</p>';
@@ -113,22 +128,41 @@ $(document).ready(function(){
             
             // click "clear search results" to empty search results entirely
             $('#clear-results').on('click', function(){
-                console.log('clearing results');
+                removeAllOutlines();
                 $('#search-results').empty();
             });
             
             // show feature boundary upon hover
-            $('.search-result').on('mouseover', function(){
+            $('.search-result').on('click', function(){
+                removeAllOutlines();
                 var featureId = $(this).attr('id');
-                geodexQuery
+                var geodexBoundsQuery = L.esri.query({
+                    url: 'http://webgis.uwm.edu/arcgisuwm/rest/services/AGSL/GeodexWebMapService/MapServer/0'
+                });
+                geodexBoundsQuery
                     .where('"OBJECTID" = '+ featureId);
-                geodexQuery.run(function(error, featureToDisplay, response){
+                geodexBoundsQuery.run(function(error, featureToDisplay, response){
                     var thisFeaturesGeometry = featureToDisplay.features[0].geometry.coordinates[0];
-                    console.log(featureToDisplay.features[0]);
-                    L.polygon(thisFeaturesGeometry, {color: 'red'}).addTo(theMap);
+                    for (i = 0; i < thisFeaturesGeometry.length; i++) {
+                        thisFeaturesGeometry[i].reverse();
+					}
+                temporaryLayer = L.polygon(thisFeaturesGeometry, {color: 'red'});
+                temporaryLayerGroup = L.layerGroup([temporaryLayer]);
+                temporaryLayerGroup.addTo(theMap);
+                outlineOnMap = true;
                 });
             });
-            
+        }
+        
+        /////////////////////////////////////////////////
+        // remove feature outline on map               //
+        /////////////////////////////////////////////////
+        
+        function removeAllOutlines() {
+            if(outlineOnMap) {
+                temporaryLayerGroup.remove();
+                outlineOnMap = false;
+            }
         }
         
     }
